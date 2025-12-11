952a44d3066a6b839f1d91a0fb0c616ec911bbcd
jayabro290-sketch <jayabro290@gmail.com>
2025-12-11 02:33:12 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "", // Ice~A5XwDE "*"
+  OWNER_NUMBER: "263784263228", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
+// CREATED BY DARRELL MUCHERI ( MR abro OFC 🇿🇼 )
